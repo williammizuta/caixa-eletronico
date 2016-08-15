a3419cffbc96b93c5e75bfb8fa23359f05a6af96
William Seiti Mizuta <login+williammizuta@elo7.com>
2016-08-15 02:26:25 -0300

    Criando uma classe separada que contém as informações da cedula e da quantidade que vai ser sacada

--- caixa-eletronico.js
+++ caixa-eletronico.js
@@ -1,12 +1,23 @@
+class NotaSaque {
+	constructor(cedula, quantidade) {
+		this.cedula = cedula;
+		this.quantidade = quantidade;
+	}
+
+	imprime() {
+		if(this.quantidade > 1) {
+			return `${this.quantidade} notas de R\$${this.cedula},00`;
+		}
+		return `${this.quantidade} nota de R\$${this.cedula},00`;
+	}
+}
+
 module.exports = {
 	saca: function(valor) {
 		const notas = [100, 50, 20, 10];
 
 		return notas.map(nota => {
-			let notaSaque = {
-				valor: nota,
-				quantidade: Math.floor(valor / nota)
-			}
+			let notaSaque = new NotaSaque(nota, Math.floor(valor / nota));
 			valor %= nota;
 			return notaSaque;
 		}).filter(nota => {
@@ -15,11 +26,7 @@ module.exports = {
 			if(indice !== 0 && indice === notas.length - 1) {
 				resultado += ' e';
 			}
-			resultado += ` ${nota.quantidade} nota`;
-			if(nota.quantidade > 1) {
-				resultado += 's';
-			}
-			return `${resultado} de R\$${nota.valor},00`;
+			return resultado + ' ' + nota.imprime();
 		}, "Entregar") + '.';
 	}
 }
