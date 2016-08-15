71805750b2a65098ce58e54f608caa7abb6e0917
William Seiti Mizuta <login+williammizuta@elo7.com>
2016-08-15 02:29:53 -0300

    Criando testes até saque de R00

--- test.js
+++ test.js
@@ -46,4 +46,44 @@ describe('Caixa eletrônico', function() {
 	it('libera saque de R$100', function() {
 		caixa.saca(100).should.equal('Entregar 1 nota de R$100,00.');
 	});
+
+	it('libera saque de R$110', function() {
+		caixa.saca(110).should.equal('Entregar 1 nota de R$100,00 e 1 nota de R$10,00.');
+	});
+
+	it('libera saque de R$120', function() {
+		caixa.saca(120).should.equal('Entregar 1 nota de R$100,00 e 1 nota de R$20,00.');
+	});
+
+	it('libera saque de R$130', function() {
+		caixa.saca(130).should.equal('Entregar 1 nota de R$100,00 1 nota de R$20,00 e 1 nota de R$10,00.');
+	});
+
+	it('libera saque de R$140', function() {
+		caixa.saca(140).should.equal('Entregar 1 nota de R$100,00 e 2 notas de R$20,00.');
+	});
+
+	it('libera saque de R$150', function() {
+		caixa.saca(150).should.equal('Entregar 1 nota de R$100,00 e 1 nota de R$50,00.');
+	});
+
+	it('libera saque de R$160', function() {
+		caixa.saca(160).should.equal('Entregar 1 nota de R$100,00 1 nota de R$50,00 e 1 nota de R$10,00.');
+	});
+
+	it('libera saque de R$170', function() {
+		caixa.saca(170).should.equal('Entregar 1 nota de R$100,00 1 nota de R$50,00 e 1 nota de R$20,00.');
+	});
+
+	it('libera saque de R$180', function() {
+		caixa.saca(180).should.equal('Entregar 1 nota de R$100,00 1 nota de R$50,00 1 nota de R$20,00 e 1 nota de R$10,00.');
+	});
+
+	it('libera saque de R$190', function() {
+		caixa.saca(190).should.equal('Entregar 1 nota de R$100,00 1 nota de R$50,00 e 2 notas de R$20,00.');
+	});
+
+	it('libera saque de R$200', function() {
+		caixa.saca(200).should.equal('Entregar 2 notas de R$100,00.');
+	});
 });
