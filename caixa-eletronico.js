fd7b0a416691b954fc82ee9087a0cbc30cebaaf9
William Seiti Mizuta <login+williammizuta@elo7.com>
2016-08-15 00:56:43 -0300

    Podendo sacar R0

--- caixa-eletronico.js
+++ caixa-eletronico.js
@@ -1,5 +1,5 @@
 module.exports = {
 	saca: function(valor) {
-
+		return 'Entregar 1 nota de R$10,00'
 	}
 }
--- test.js
+++ test.js
@@ -6,4 +6,8 @@ describe('Caixa eletrônico', function() {
 	it('possui a função saca', function() {
 		caixa.should.have.property('saca');
 	});
+
+	it('libera saque de R$10', function() {
+		caixa.saca(10).should.equal('Entregar 1 nota de R$10,00');
+	});
 });
